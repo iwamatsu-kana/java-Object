ce547dfaa47133b5019de1a2422cc637700ba442
iwamatsu-kana <2828cdmpsakura@au.com>
2024-01-07 22:50:06 +0900

    演習②　インスタンス化　鈴木花子

--- Person/src/Test.java
+++ Person/src/Test.java
@@ -3,8 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 Person taro = new Person();
-taro.name = "木村次郎";
-taro.age = 18;
+taro.name = "鈴木花子";
+taro.age = 16;
 
 System.out.println(taro.name);
 System.out.println(taro.age);
