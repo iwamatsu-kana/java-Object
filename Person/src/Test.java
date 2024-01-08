4f8263bc3577f41635e295a13f2760eb9133173c
iwamatsu-kana <2828cdmpsakura@au.com>
2024-01-08 11:38:31 +0900

    演習⑤と⑥を追加しました

--- Person/src/Test.java
+++ Person/src/Test.java
@@ -3,11 +3,13 @@ public class Test {
 
 	public static void main(String[] args) {
 Person taro = new Person();
-taro.name = "岩松佳奈";
-taro.age = 24;
+taro.name = "山田太郎";
+taro.age = 20;
 
 System.out.println(taro.name);
 System.out.println(taro.age);
+taro.talk();
+taro.walk();
 
 	}
 
