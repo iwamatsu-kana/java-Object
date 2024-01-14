84d8ea7e1d75fee5f8e33748d02e5c6b0e48a77c
iwamatsu-kana <2828cdmpsakura@au.com>
2024-01-14 16:20:15 +0900

    01_Java オブジェクト指向（インスタンス化）完了しました。

--- Person/src/Test.java
+++ Person/src/Test.java
@@ -2,14 +2,14 @@
 public class Test {
 
 	public static void main(String[] args) {
-Person asimo = new Person();
-asimo.name = "山田太郎";
-asimo.age = 20;
+Person pepper = new Person();
+pepper.name = "山田太郎";
+pepper.age = 20;
 
-System.out.println(asimo.name);
-System.out.println(asimo.age);
-asimo.talk();
-asimo.walk();
+System.out.println(pepper.name);
+System.out.println(pepper.age);
+pepper.talk();
+pepper.walk();
 
 	}
 
