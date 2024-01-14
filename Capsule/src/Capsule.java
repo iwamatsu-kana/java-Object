6fde64515b18e7279353a48f676fac342efbfb1b
iwamatsu-kana <2828cdmpsakura@au.com>
2024-01-14 15:20:42 +0900

    演習⑤を追加しました

--- Capsule/src/Capsule.java
+++ Capsule/src/Capsule.java
@@ -6,7 +6,9 @@ public class Capsule {
 		System.out.println(taro.getName());
 		taro.setName("花子");
 		System.out.println(taro.getName());
-
+		System.out.println(taro.getAge());
+		taro.setAge(30);
+		System.out.println(taro.getAge());
 	}
 
 }
